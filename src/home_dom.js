86aa3cf937291a6c0cd336ca33565cd3d20f9b01
Andrei Quirante <andrei.quirante@ciit.edu.ph>
2023-11-07 09:33:41 +0800

    rain

--- src/home_dom.js
+++ src/home_dom.js
@@ -14,6 +14,9 @@ window.onload = function () {
   Footer()
   Welcome()
   Watermark();
+
+  const windowHeight = 900;
+  CreateRaindrops(30, windowHeight);
 }
 function Header () {
   var header = document.getElementById('header')
@@ -271,6 +274,38 @@ function Watermark(){
   watermark.style.fontSize = '12px';
 }
 
-function Rain(){
-  
+function createRaindrop(maxHeight) {
+  const raindrop = document.createElement('div');
+  raindrop.className = 'raindrop';
+  raindrop.style.width = '1.5px';
+  raindrop.style.height = `${Math.random() * 20 + 10}px`;
+  raindrop.style.backgroundColor = '#828282';
+  raindrop.style.position = 'absolute';
+  const leftPosition = Math.random() * (window.innerWidth - 2);
+  raindrop.style.left = `${leftPosition}px`;
+  document.body.appendChild(raindrop);
+
+  document.getElementById('rain').style.zIndex = '3';
+  let position = 0;
+  const fallSpeed = 10;
+
+  function fall() {
+    position += fallSpeed;
+    raindrop.style.top = position + 'px';
+
+    if (position < maxHeight) {
+      requestAnimationFrame(fall);
+    } else {
+      raindrop.remove();
+      setTimeout(() => createRaindrop(maxHeight), Math.random() * 3000); 
+    }
+  }
+
+  fall();
+}
+
+function CreateRaindrops(numRaindrops, maxHeight) {
+  for (let i = 0; i < numRaindrops; i++) {
+    setTimeout(() => createRaindrop(maxHeight), Math.random() * 3000);
+  }
 }
